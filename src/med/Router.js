092a0dcec5859d5308b8d18f8c165838c51ce243
anna <donot459@gmail.com>
2023-02-27 10:08:12 +0900

    Add: 회원가입 페이지

--- src/med/Router.js
+++ src/med/Router.js
@@ -6,16 +6,18 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Main from "./Main";
 import Login from "./Login";
+import SignUp from "./SignUp";
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Login/>}></Route> {/**/}
+                <Route exact path="/" element={<Login/>}></Route> {/*홈페이지 접근 시 보여줄 첫 화면*/}
                 <Route path="Main" element={<Main/>}></Route> {/*메인*/}
                 <Route path="Login" element={<Login/>}></Route> {/*로그인*/}
-                <Route path="SignUp" element={<Login/>}></Route> {/*회원가입*/}
+                <Route path="SignUp" element={<SignUp/>}></Route> {/*회원가입*/}
                 <Route path="FindPw" element={<Login/>}></Route> {/*아이디/비밀번호 찾기*/}
+                <Route element={<notFound/>}></Route> {/*접근거부*/}
             </Routes>
         </BrowserRouter>
     );
