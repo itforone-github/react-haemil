28158c2a43414519e151246f579dd4ba88833d2f
anna <donot459@gmail.com>
2023-03-06 16:43:03 +0900

    Project Init

---
+++ pages/_app.js
@@ -0,0 +1,51 @@
+/**
+ * 페이지 초기화
+ * 서버 요청 시 가장 먼저 실행되며 모든 페이지에 공통적으로 적용
+ * @Component: 서버에 요청한 페이지
+ */
+import PropTypes from "prop-types";
+import {useEffect} from "react";
+import Script from "next/script";
+// css
+import "/styles/bootstrap.min.css";
+import "/styles/all.min.css";
+import "/styles/sideber.css";
+import "/styles/common.css";
+import "/styles/member.css";
+import "/styles/layout.css";
+import "/styles/med.css";
+
+const HamilApp = ({Component, pageProps}) => {
+    // 페이지 렌더링 후 실행 (useEffect(function, deps) => deps 생략 시 렌더링 될 때 마다 실행)
+    useEffect(() => {
+        const script1 = document.createElement("script");
+        script1.src = "/utils/jquery-1.9.1.min.js";
+        const script2 = document.createElement("script");
+        script2.src = "/utils/ui.js";
+        document.body.appendChild(script1);
+        document.body.appendChild(script2);
+        return () => {
+            document.body.removeChild(script1);
+            document.body.removeChild(script2);
+        }
+    });
+
+    return (
+        <>
+            <Component {...pageProps}/>
+
+            {/*<Script src="/utils/jquery-1.9.1.min.js"></Script>
+            <Script src="/utils/bootstrap.min.js" strategy={"lazyOnload"}></Script>
+            <Script src="/utils/ui.js" strategy={"lazyOnload"}></Script>
+            <Script src="/utils/wow.min.js" strategy={"lazyOnload"}></Script>
+            <Script src="/utils/swiper.min.js" strategy={"lazyOnload"}></Script>*/}
+        </>
+    )
+};
+
+HamilApp.propTypes = {
+    Component: PropTypes.elementType.isRequired,
+    // pageProps: PropTypes.elementType.isRequired
+}
+
+export default HamilApp;
---
+++ public/utils/ui.js
@@ -0,0 +1,113 @@
+
+$(document).ready(function(){
+  /*/체크박스디자인 div추가
+  $('input[type=checkbox] + label').prepend('<div></div>');
+  $('input[type=radio] + label').prepend('<div></div>');
+  */
+
+  //사이드바
+  $(function(){
+    $(".lnb > div:not("+$(".side-icon > a.active").attr("href")+")").hide();
+    $(".side-icon > a").click(function(){
+      $(".side-icon > a").removeClass("active");
+      $(this).addClass("active");
+      $(".lnb > div").slideUp(200);
+      $($(this).attr("href")).slideDown(200);
+      return false;
+      })
+  })
+
+  $(".sidebar-dropdown > a").click(function() {
+    $(".sidebar-submenu").slideUp(200);
+    if (
+        $(this)
+            .parent()
+            .hasClass("active")
+    ) {
+      $(".sidebar-dropdown").removeClass("active");
+      $(this)
+          .parent()
+          .removeClass("active");
+    } else {
+      $(".sidebar-dropdown").removeClass("active");
+      $(this)
+          .next(".sidebar-submenu")
+          .slideDown(200);
+      $(this)
+          .parent()
+          .addClass("active");
+    }
+  });
+
+  $("#close-sidebar").click(function() {
+    $(".page-wrapper").removeClass("toggled");
+  });
+  $("#show-sidebar").click(function() {
+    $(".page-wrapper").addClass("toggled");
+  });
+
+  //사이드바 
+  var w = $(window).width();
+	if(w < 950){
+      $(".page-wrapper").removeClass("toggled");
+	}
+  //이미지 업로드
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        $('#imagePreview').css('background-image', 'url('+e.target.result +')');
+        $('#imagePreview').hide();
+        $('#imagePreview').fadeIn(650);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+  $("#imageUpload").change(function() {
+    readURL(this);
+  });
+  //닫기
+  $('button.slideup1').click( function() {
+    $('div.slideform1').slideToggle();
+    if( $(this).html() == '닫기' ) {
+      $(this).html('열기');
+    }
+    else {
+      $(this).html('닫기');
+    }
+  } );
+  $('button.slideup2').click( function() {
+    $('div.slideform2').slideToggle();
+    if( $(this).html() == '닫기' ) {
+      $(this).html('열기');
+    }
+    else {
+      $(this).html('닫기');
+    }
+  } );
+  $('button.slideup3').click( function() {
+    $('div.slideform3').slideToggle();
+    if( $(this).html() == '닫기' ) {
+      $(this).html('열기');
+    }
+    else {
+      $(this).html('닫기');
+    }
+  } );
+  //탭메뉴
+
+    $('ul.tabs li').click(function(){
+      var tab_id = $(this).attr('data-tab');
+
+      $('ul.tabs li').removeClass('current');
+      $('.tab-content').removeClass('current');
+
+      $(this).addClass('current');
+      $("#"+tab_id).addClass('current');
+    })
+
+
+});
+
+
+
